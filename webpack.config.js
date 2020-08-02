a0b0f98c1f89b73a6ae055295f1f1f63b206cef2
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-08-02 21:37:39 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// This is the main configuration object.
-// Here you write different options and tell Webpack what to do
+
 module.exports = {
 
-  // Path to your entry point. From this file Webpack will begin his work
+  // Entry point
   entry: './src/media/js/index.js',
 
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
+  // Output
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -77,13 +75,8 @@ module.exports = {
 
     new MiniCssExtractPlugin({
       filename: "bundle.css"
-    })
-  
+    }) 
   ],
 
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript 
-  // minifying and other thing so let's set mode to development
   mode: 'development'
 };
