91322855654c12620970f70d1ffc76925d866fe1
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-12-26 15:05:19 +0100

    added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2020": true,
-        "node": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 11
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['standard', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {},
 };
