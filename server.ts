e4885ea671a5c2b0572d7b2ee6168ba93c45d51f
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-09-25 15:59:11 +0200

    fix on package

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import helmet from "helmet";
-import cors from "cors";
+import express from 'express';
+import path from 'path';
+import * as dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
 
 // ? import routes
 import { router } from './src/routes/router'
@@ -19,6 +20,13 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.resolve(__dirname, './client/build'))); //? this will make react app visible to server
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 //? use all routes
 app.use('/', router);
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,9 @@
+import { Router } from 'express';
 import express, { Request, Response } from "express"
 
-export const router = express.Router();
+export const router = Router(); //? declare express as router
 
+//? api endpoint
 router.get("/api", async (req: Request, res: Response) => {
     try {
         res.status(200).send(res.json({ message: 'Welcome to Andromeda' }));
