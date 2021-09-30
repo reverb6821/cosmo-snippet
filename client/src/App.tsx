7bfaf585b651572f2b7d827c12301d291c18293c
reverb6821 <giuseppegigliottidev@gmail.com>
2021-09-30 12:11:19 +0200

    added error 404 on react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
-import Home from './Components/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import ErrorHandler from './lib/errorHandler';
+
+import Home from './Pages/Home';
+import Page404 from './Pages/Page404';
 
 function App() {
-  return <Home />;
+    return (
+        <BrowserRouter>
+            <ErrorHandler>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route component={Page404} />
+                </Switch>
+            </ErrorHandler>
+        </BrowserRouter>
+    );
 }
 
 export default App;
