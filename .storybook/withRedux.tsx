9ba69d07a705c185099c58a75cf6ff12a701fc8d
reverb6821 <giuseppegigliottidev@gmail.com>
2024-03-09 11:58:55 +0100

    perf: wiped repo

--- .storybook/withRedux.tsx
+++
@@ -1,9 +0,0 @@
-import { Provider } from 'react-redux';
-import store from '../src/store/store';
-import * as React from 'react';
-
-export const withRedux = (Story) => (
-    <Provider store={store}>
-      <Story />
-    </Provider>
-  );
